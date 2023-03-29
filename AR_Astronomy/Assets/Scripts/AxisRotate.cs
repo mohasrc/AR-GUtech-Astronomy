b7364eba43d89ffd23568699fc1f886740999866
LiqaMA <127586772+LiqaMA@users.noreply.github.com>
2023-03-29 16:22:47 +0300

    axis rotate for planets

---
+++ AR_Astronomy/Assets/Scripts/AxisRotate.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AxisRotate : MonoBehaviour
+{
+    public float speed = 1f;
+    public bool rotate = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotate)
+        {
+            Vector3 direction = new Vector3(0, Time.deltaTime * speed, 0);
+            direction.Normalize();
+            transform.Rotate(direction * speed);
+        }
+    }
+
+    public void RotateState(bool enable)
+    {
+        rotate = enable;
+    }
+}
