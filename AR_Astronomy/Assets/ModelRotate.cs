d9a20a5dfde3a37b90cfa75a11cf02965ea26a05
mallaksh <mallak.balushi@gmail.com>
2023-04-15 03:20:58 +0400

    Scale and Rotate Sliders + Navigation By Touch

---
+++ AR_Astronomy/Assets/ModelRotate.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class RotateObject : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject rotatedObject;
+    public float rotationSpeed = 20;
+    bool rotate = false;
+
+    void FixedUpdate()
+    {
+        if (rotate == false)
+            return;
+
+        rotatedObject.transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        rotate = true;
+    }
+
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        rotate = false;
+    }
+}
