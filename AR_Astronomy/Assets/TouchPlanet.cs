38a123b3bb85cfd99aea5fa6156d0e55b5f1d935
mallaksh <mallak.balushi@gmail.com>
2023-06-05 14:56:32 +0400

    rotate planet on

--- AR_Astronomy/Assets/TouchPlanet.cs
+++ AR_Astronomy/Assets/TouchPlanet.cs
@@ -18,32 +18,27 @@ public class TouchPlanet : MonoBehaviour
     {
         if (isActive)
         {
-           
             if (Input.touchCount == 1)
             {
                 Touch screenTouch = Input.GetTouch(0);
 
+                //tap to show description
                 if (screenTouch.phase == TouchPhase.Began)
                 {
-                
                     show = true;
                     panel.SetActive(show);
-                
                 }
 
+                //move finger to rotate the planet on its axis
                 if (screenTouch.phase == TouchPhase.Moved)
                 {
                     transform.Rotate(0f, screenTouch.deltaPosition.x, 0f);
-                    
-
                 }
                 if (screenTouch.phase == TouchPhase.Ended)
                 {
                     isActive = false;
-                    
                 }
             }
         }
-       
     }
 }
